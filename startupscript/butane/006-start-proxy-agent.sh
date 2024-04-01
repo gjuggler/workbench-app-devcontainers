f2f087c85347dc72a1800e236da5660e3a754fb4
Greg Jordan <gjuggler@gmail.com>
2024-04-01 12:39:03 -0400

    Misc tweaks.

--- startupscript/butane/006-start-proxy-agent.sh
+++ startupscript/butane/006-start-proxy-agent.sh
@@ -24,7 +24,7 @@ if [[ -z "${PORT}" ]]; then
     exit 1
 fi
 
-echo "Proxy agent port should listen at port ${PORT}"
+echo "Proxy agent3 port should listen at port ${PORT}"
 
 # shellcheck source=/dev/null
 source /home/core/agent.env
